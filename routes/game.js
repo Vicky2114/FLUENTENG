838ab4c4c1f14162f061462827a17860572f074d
unknown <vr6994056@gmail.com>
2023-03-04 08:44:27 +0530

    hi

--- routes/game.js
+++ routes/game.js
@@ -6,11 +6,9 @@ const passport = require('passport');
 const game=require('../controllers/game_controller');
 
 router.get('/game',passport.checkAuthentication,game.game);
-<<<<<<< HEAD
-=======
 router.get('/game1',passport.checkAuthentication,game.game1);
 router.get('/game2',passport.checkAuthentication,game.game2);
->>>>>>> b6d74c8d5c170ce49026ed9055c6543bec9f0400
+
 
 
 
