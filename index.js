a10a6b6541325b012e5b406a49dbb151fe74d356
Vicky2114 <vr6994056@gmail.com>
2023-03-20 16:10:13 +0530

    hello

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-const port = process.env.PORT||8000;
+const port = process.env.PORT||10000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const session = require('express-session');
