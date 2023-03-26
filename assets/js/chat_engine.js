452e8a262e08ab9e97602ade338ae7cb9b71827c
Vicky2114 <vr6994056@gmail.com>
2023-03-26 09:29:38 +0530

    26 march

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('https://fluenteng.onrender.com/');
+        this.socket = io.connect('https://fluenteng.onrender.com');
 
         if (this.userEmail){
             this.connectionHandler();
